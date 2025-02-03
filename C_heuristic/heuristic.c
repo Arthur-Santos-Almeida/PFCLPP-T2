f6c02f67a95b0526b17ca692f870041be147b440
Arthur-Almeida-Powwr <arthur.almeida@powwr.com>
2025-02-02 23:13:42 -0300

    irace funcionando

--- C_heuristic/heuristic.c
+++ C_heuristic/heuristic.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <stdbool.h>
+#include <time.h>
 
 // Define a struct Conflito
 typedef struct {
@@ -9,7 +11,7 @@ typedef struct {
     int j;
     int a;
     int b;
-    float jaccardIndex;
+    double jaccardIndex;
 } Conflito;
 
 void ler_N_P(const char *caminho_arquivo, int *N, int *P) {
@@ -23,7 +25,48 @@ void ler_N_P(const char *caminho_arquivo, int *N, int *P) {
     fclose(arquivo);
 }
 
-void ler_coeficientes(const char *caminho_arquivo, Conflito *conflitos, int *numConflitosGerados, int N, int P) {
+int ler_num_conflitos(const char *caminho_arquivo, int N, int P) {
+    FILE *arquivo = fopen(caminho_arquivo, "r");
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        exit(EXIT_FAILURE);
+    }
+
+    // Ignora as duas primeiras linhas (já lidas em ler_N_P)
+    fscanf(arquivo, "%*d");
+    fscanf(arquivo, "%*d");
+
+    int numConflitos = 0;
+    int numConflitosTotal = 0;
+    char bufferDescartavel[2000];
+
+    for (int i = 1; i <= N; i++) {
+        for (int a = 1; a <= P; a++) {
+
+            fscanf(arquivo, "%d\n", &numConflitos);  // Lê o número de conflitos, SE VOCÊ NÃO EXPLICITA O \n O FSCANF NÃO CONSOME ESSE CARACTERE E DÁ ERRADO!!
+
+            if (numConflitos != 0) {
+                fgets(bufferDescartavel, sizeof(bufferDescartavel), arquivo);
+                fgets(bufferDescartavel, sizeof(bufferDescartavel), arquivo);
+            }
+
+            numConflitosTotal += numConflitos;
+            // if(numConflitosTotal < 20000){
+            //     printf("%d\n", numConflitosTotal);
+            //     FILE *arquivoEscrita = fopen("C:/OCII/PFCLPP/T2/escrita/c.txt", "a");
+            //     fprintf(arquivoEscrita, "%d\n", numConflitosTotal);
+            //     fclose(arquivoEscrita);
+            // }
+        }
+    }
+
+    fclose(arquivo);
+    return numConflitosTotal;
+}
+
+Conflito* ler_coeficientes(int numConflitosComRedundancia, const char *caminho_arquivo, int N, int P) {
+    Conflito *conflitos = malloc( numConflitosComRedundancia * sizeof(Conflito) );
+    
     FILE *arquivo = fopen(caminho_arquivo, "r");
     if (arquivo == NULL) {
         perror("Erro ao abrir o arquivo");
@@ -36,7 +79,7 @@ void ler_coeficientes(const char *caminho_arquivo, Conflito *conflitos, int *num
 
     int numConflitos = 0;
     int conflitoIndex = 0;
-    char bufferDescartavel[256];
+    char bufferDescartavel[2000];
 
     for (int i = 1; i <= N; i++) {
         for (int a = 1; a <= P; a++) {
@@ -62,55 +105,41 @@ void ler_coeficientes(const char *caminho_arquivo, Conflito *conflitos, int *num
                 double parte_decimal = fmod((double)idLabelEmConflito / P, 1.0);
 
                 for (int p = 1; p <= P; p++) {
-                    if (fabs(parte_decimal * ((float)P / (float)p) - 1.0) < 1e-6) {  // Compara com tolerância para evitar problemas de ponto flutuante
+                    if (fabs(parte_decimal * ((double)P / (double)p) - 1.0) < 1e-6) {  // Compara com tolerância para evitar problemas de ponto flutuante
                         int b = p + 1;
                         conflitos[conflitoIndex].i = i;
                         conflitos[conflitoIndex].j = j;
                         conflitos[conflitoIndex].a = a;
                         conflitos[conflitoIndex].b = b;
-                        conflitos[conflitoIndex].jaccardIndex = (float)jaccardIndex;
+                        conflitos[conflitoIndex].jaccardIndex = (double)jaccardIndex;
                         conflitoIndex++;
+                        break;
                     } else if (parte_decimal == 0) {  // Caso seja exato
                         int b = 1;
                         conflitos[conflitoIndex].i = i;
                         conflitos[conflitoIndex].j = j;
                         conflitos[conflitoIndex].a = a;
                         conflitos[conflitoIndex].b = b;
-                        conflitos[conflitoIndex].jaccardIndex = (float)jaccardIndex;
+                        conflitos[conflitoIndex].jaccardIndex = (double)jaccardIndex;
                         conflitoIndex++;
+                        break;
                     }
                 }
 
-                // printf("%d\n", conflitoIndex);
                 // printf("%d %d %d %d %f\n", conflitos[conflitoIndex-1].i, conflitos[conflitoIndex-1].j, conflitos[conflitoIndex-1].a, conflitos[conflitoIndex-1].b, conflitos[conflitoIndex-1].jaccardIndex);
+                // FILE *arquivoEscrita = fopen("C:/OCII/PFCLPP/T2/escrita/c.txt", "a");
+                // fprintf(arquivoEscrita, "%d %d %d %d %f\n", conflitos[conflitoIndex-1].i, conflitos[conflitoIndex-1].j, conflitos[conflitoIndex-1].a, conflitos[conflitoIndex-1].b, conflitos[conflitoIndex-1].jaccardIndex);
+                // fclose(arquivoEscrita);
             }
         }
     }
 
-    *numConflitosGerados = conflitoIndex;  // Atualiza o total de conflitos gerados
     fclose(arquivo);
+    return conflitos;
 }
 
 bool conflito_igual(Conflito c1, Conflito c2) {
     return c1.i == c2.i && c1.j == c2.j && c1.a == c2.a && c1.b == c2.b && c1.jaccardIndex == c2.jaccardIndex;
-}
-
-int remover_duplicatas(Conflito listaConflitos[], int tamanho, Conflito resultado[]) {
-    int novoTamanho = 0;
-
-    for (int i = 0; i < tamanho; i++) {
-        bool encontrado = false;
-        for (int j = 0; j < novoTamanho; j++) {
-            if (conflito_igual(listaConflitos[i], resultado[j])) {
-                encontrado = true;
-                break;
-            }
-        }
-        if (!encontrado) {
-            resultado[novoTamanho++] = listaConflitos[i];
-        }
-    }
-    return novoTamanho;
 }
 
 int remover_redundantes(Conflito listaConflitos[], int tamanho) {
@@ -143,31 +172,160 @@ int remover_redundantes(Conflito listaConflitos[], int tamanho) {
     return novoTamanho;
 }
 
-int main() {
-    const char *nome_instancia = "h2_w24_58_all";
-    char caminho_arquivo[100];
-    snprintf(caminho_arquivo, sizeof(caminho_arquivo), "../Instances/%s.confl", nome_instancia);
+int* construtiva_aleatoria(int numPontos) {
+
+    int *solucao = malloc(numPontos * sizeof(int));
+
+    for (int i = 0; i < numPontos; i++) {
+        solucao[i] = rand() % 4;
+    }
+
+    return solucao;
+}
+
+double calc_FO(int numConflitos, Conflito *conflitos, int *solucao) {
+    // Nota: em "conflitos" i, j, a, b começam de "1",
+    // Já em "solucao", i é representado pelas posições do vetor, que começa em "0".
+    // Ainda em "solucao, as posições p começam de "0".
+
+    double FO = 0;
+    for( int c = 0; c < numConflitos; c++ ) {
+        int i = conflitos[c].i;
+        int j = conflitos[c].j;
+        int a = conflitos[c].a - 1;
+        int b = conflitos[c].b - 1;
+
+        if( solucao[i] == a && solucao[j] == b ) {
+            FO += conflitos[c].jaccardIndex;
+        }
+    }
+
+    return FO;
+}
+
+void perturba_solucao(int numPontos, int* solucao, int maxPorCentoPerturbacoes) {
+    // Pega um ponto "i" aleatório e troca sua posição por uma NOVA posição aleatória
+
+    int porCentoPerturbacoes = rand() % maxPorCentoPerturbacoes;
+    int numPontosPerturbados = ceil( numPontos * (0.01 * porCentoPerturbacoes) );
+
+    for( int i = 0; i < numPontosPerturbados; i++ ) {
+
+        int indexPonto = rand() % numPontos;
+        int velhaPosicaoDoPonto = solucao[indexPonto];
+
+        while(solucao[indexPonto] == velhaPosicaoDoPonto) {
+            solucao[indexPonto] = rand() % 4;
+        }
+    }
+}
+
+void printa_solucao(int *solucao, int numPontos) {
+
+    printf("Solucao:\n");
+    for( int i = 0; i < numPontos; i++ ) {
+        printf("%d ", solucao[i]);
+    }
+
+    printf("\n");
+}
+
+double simmulated_annealing(float taxaResfriamento, int SAMax, float temperaturaInicial, float temperaturaCongelamento, int *solucao, int numConflitos, Conflito *conflitos, int numPontos, int kPerturbacoes) {
+    calc_FO(numConflitos, conflitos, solucao);
+
+    int *solucaoMelhor = malloc(numPontos * sizeof(int));
+    memcpy(solucaoMelhor, solucao, numPontos * sizeof(int));
+    double FOMelhor = calc_FO(numConflitos, conflitos, solucaoMelhor);
+
+    int iterT = 0;
+    int temperatura = temperaturaInicial;
+    
+    while( temperatura > temperaturaCongelamento ) {
+
+        while( iterT < SAMax ) {
+            iterT++;
+
+            // Gerar um vizinho
+            int *solucaoDepois = malloc(numPontos * sizeof(int));
+            memcpy(solucaoDepois, solucao, numPontos * sizeof(int));
+            perturba_solucao(numPontos, solucaoDepois, kPerturbacoes);
+
+            double FO = calc_FO(numConflitos, conflitos, solucao);
+            double FODepois = calc_FO(numConflitos, conflitos, solucaoDepois);
+            double delta = FODepois - FO;
+
+            // Delta negativo: FODepois é menor (e melhor) que FO
+            if( delta < 0 ) {
+                solucao = solucaoDepois;
+
+                if( FODepois < FOMelhor ) {
+                    solucaoMelhor = solucaoDepois;
+                    FOMelhor = FODepois;
+                }
+            }
+            else {
+                // Como RAND_MAX é o maior valor aleatório possível, dividir por RAND_MAX garante que o resultado seja entre 0 e 1
+                double x = (double)rand() / RAND_MAX;
+                double probabilidade = exp(-delta / temperatura);
+
+                if( x < probabilidade ) {
+                    solucao = solucaoDepois;
+                }
+            }
+        }
+
+        temperatura = taxaResfriamento * temperatura;
+        iterT = 0;
+    }
+
+    return FOMelhor;
+}
+
+int main(int argc, char *argv[]) {
+
+    // Início do relógio
+    clock_t start = clock();
+
+    if (argc < 6) {
+        // Exemplo de uso: .\heuristic.exe h2_w24_58_all 0.99 200 10 0.01 50
+        printf("Uso: %s <instancia> <taxaResfriamento> <SAMax> <temperaturaInicial> <temperaturaCongelamento> <kPerturbacoes>\n", argv[0]);
+        // return 1;
+    }
+
+    /* Parâmetros recebidos por argumento na linha de comando */
+    const char *instancia = argv[1];
+    float taxaResfriamento = atof(argv[2]);  // Entre 0 e 1
+    int SAMax = atoi(argv[3]); // Número máximo de iterações por temperatura
+    float temperaturaInicial = atof(argv[4]); 
+    float temperaturaCongelamento = atof(argv[5]);// Próxima a 0 (0.01, 0.001)
+    int kPerturbacoes = atoi(argv[6]); // Número de pontos que eu perturbo a posição do label
+
+    /* Descomente as linhas abaixo para setar os parâmetros em código. Facilita a debugar o algoritmo. */
+    // const char *instancia = "h2_w24_58_all";
+    // float taxaResfriamento = 0.99;  // Entre 0 e 1
+    // int SAMax = 200; // Número máximo de iterações por temperatura
+    // float temperaturaInicial = 10; 
+    // float temperaturaCongelamento = 0.01;// Próxima a 0 (0.01, 0.001)
+    // int kPerturbacoes = 50; // Máxima porcentagem de pontos a qual poderão ser perturbados a posição do label
 
     int N, P;
+    char caminho_arquivo[100];
+    snprintf(caminho_arquivo, sizeof(caminho_arquivo), "./Instances/%s.confl", instancia);
+
+    srand(time(NULL));
+
     ler_N_P(caminho_arquivo, &N, &P);
+    int numConflitosComRedundancia = ler_num_conflitos(caminho_arquivo, N, P);
+    Conflito *conflitos = ler_coeficientes(numConflitosComRedundancia, caminho_arquivo, N, P);
+    int numConflitos = remover_redundantes(conflitos, numConflitosComRedundancia);
+    int *solucao = construtiva_aleatoria(N); 
+    double FO = simmulated_annealing(taxaResfriamento, SAMax, temperaturaInicial, temperaturaCongelamento, solucao, numConflitos, conflitos, N, kPerturbacoes);
 
-    Conflito conflitos[2000];  // Capacidade máxima arbitrária para os conflitos
-    int numConflitosGerados = 0;
+    // Fim do relógio
+    clock_t end = clock();
+    double tempo_exec = (double)(end - start) / CLOCKS_PER_SEC;
 
-    ler_coeficientes(caminho_arquivo, conflitos, &numConflitosGerados, N, P);
-
-    Conflito *conflitosUnicos = (Conflito*) malloc( numConflitosGerados * sizeof(Conflito) );
-    int tamanhoSemDuplicatas = remover_duplicatas(conflitos, numConflitosGerados, conflitosUnicos);
-    int tamanhoFinal = remover_redundantes(conflitosUnicos, tamanhoSemDuplicatas);
-
-    // Exibe a lista de conflitos
-    for (int i = 0; i < tamanhoFinal; i++) {
-        printf("[%d, %d, %d, %d, %.6f]\n",
-               conflitosUnicos[i].i, conflitosUnicos[i].j,
-               conflitosUnicos[i].a, conflitosUnicos[i].b,
-               conflitosUnicos[i].jaccardIndex);
-    }
-    printf("\nTotal de conflitos gerados: %d\n", tamanhoFinal);
+    printf("%.6f %.6f\n", FO, tempo_exec);
 
     return 0;
 }
